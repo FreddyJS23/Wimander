c1e67627feaace1e84ee63b38482cb1a3fd356a0
Freddy Solarte <freddyjsolarte@gmail.com>
2023-06-08 15:52:54 -0400

    Correcion de estilo de login,cambio de animaciones
    Se quito el el tipo de animacion resorte a los modales para mejor rendimineto
    
    Se agrego espaciado extra a los campos del formulario de registro

--- src/components/AnimacionModal.tsx
+++ src/components/AnimacionModal.tsx
@@ -14,10 +14,10 @@ export const AnimacionModal = ({
     <AnimatePresence>
       {open && (
         <motion.div
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0 }}
-          transition={{ type: "spring", duration: 0.8 }}
+          initial={{ opacity: 0, y:-20 }}
+          animate={{ opacity: 1, y:0 }}
+          exit={{ opacity: 0, y:-20, }}
+          transition={{  duration: 0.3 }}
         >
           {children}
         </motion.div>
