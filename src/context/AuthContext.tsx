3c4fd8396552a238667ceace1e7cc53865136688
Freddy Solarte <freddyjsolarte@gmail.com>
2023-07-27 12:59:30 -0400

    refactor: change de logica en contexto
    se cambio las variables que se tenian en el contexto global

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,10 @@
 import { createContext, useState } from "react";
 import { AuthContextInterface, User } from "../types";
 
+
 const initialContext: AuthContextInterface = {
-  user: { user: "", token: "" },
-  handleLogin: function (): void {
-    throw new Error("Function not implemented.");
-  },
-  handleLogout: function (): void {
-    throw new Error("Function not implemented.");
-  },
+ initialStateUser:{ id: "",name:"", token: "" },
+ setUser:() => void{}
 };
 
 interface Props {
@@ -17,24 +13,17 @@ interface Props {
 /* los contexto se tienen que tipar con lo que se vaya enviar 
 en caso de no tener uno inicial se inicia undefined */
 //Crear contexto
-export const AuthContext = createContext<AuthContextInterface>(initialContext);
+export const AuthContext = createContext<AuthContextInterface >(initialContext);
 
 AuthContext.displayName = "Context Display Name";
 //Crear Provider
 const AuthProvider = ({ children }: Props) => {
   //operaciones que se quieren tener en un contexto global
-  const initialState = { user: "", token: "" };
-  const [user, setUser] = useState<User>(initialState);
+  const initialStateUser = {id: "",name:"", token: "" };
+  const [user, setUser] = useState<User>(initialStateUser);
 
-  const handleLogin = () => {
-    setUser({ ...user, token: "login" });
-  };
-
-  const handleLogout = () => {
-    setUser({ ...user, token: "" });
-  };
   //Enviar operaciones al provider, debe ir tipadas dichas operaciones a enviar
-  const auth: AuthContextInterface = { user, handleLogin, handleLogout };
+  const auth: AuthContextInterface = { user,setUser,initialStateUser};
 
   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
