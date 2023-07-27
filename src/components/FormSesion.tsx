bdb476770815722e65c48d2618959315ba5cfd50
Freddy Solarte <freddyjsolarte@gmail.com>
2023-07-27 12:56:06 -0400

    feat: add uso de react-hook-form y alertas
    uso de react hook form
    
    envio de formulario y obtencion de respuesta
    
    control de error para mostrar alertas en caso de error

--- src/components/FormSesion.tsx
+++ src/components/FormSesion.tsx
@@ -4,60 +4,83 @@ import styles from "../styles/login.module.css";
 import flechaButton from "../assets/right-arrow.svg";
 import Button from "./Button";
 import { AuthContext } from "../context/AuthContext";
-
+import {  UserForm } from "../types";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Autenticar } from "../services/auth";
+import Alerts from "./Alerts";
 
 interface Props {
   handleClick: () => void;
 }
 
 const FormSesion = ({ handleClick }: Props) => {
-  const initialState = { usuario: "", password: "" };
+  const { setUser, user } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserForm>();
 
-  const [form, setForm] = useState(initialState);
+  //estado de error de alerta
+  const [error, setError] = useState({ error: false, mensaje: "" });
 
-  const {handleLogin} = useContext(AuthContext);
+  //logica validacion de usuario
+  const onSubmit: SubmitHandler<UserForm> = async (form) => {
+    const { data, status } = await Autenticar(form);
+   
+    if (status == 200) setUser(data.user);
+   
+    else if (status == 408) setError({ error: true, mensaje: `${status}: error de conexion` });
+   
+    else setError({ error: true, mensaje: data.message });};
+ 
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    setForm({ ...form, [name]: value });
+  //cierre de alerta
+  const onClose = () => {
+    setError({ ...error, error: false });
   };
-//logica validacion de usuario
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    handleLogin();
-  };
-
+ 
   return (
-    <form
-      action=""
-      className={styles["formLoginInicio"]}
-      onSubmit={handleSubmit}
-    >
-      <InputLogin
-        handleChange={handleChange}
-        value={form.usuario}
-        id="usuario"
-        name="Usuario"
-        type="text"
-        styleLabel="labelLogin"
-      />
-      <InputLogin
-        handleChange={handleChange}
-        value={form.password}
-        id="password"
-        name="Contraseña"
-        type="password"
-        styleLabel="labelLogin"
-      />
-      <div className={styles["container-buttons"]}>
-        <Button type={"submit"} value="Iniciar" style="buttonIniciarSesion">
-          <img src={flechaButton} alt="icono flecha derecha" />{" "}
-        </Button>
+    <>
+      <form
+        action=""
+        className={styles["formLoginInicio"]}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <InputLogin
+          maxLength={20}
+          minLength={3}
+          required={true}
+          inputName="user"
+          name="Usuario"
+          type="text"
+          styleLabel="labelLogin"
+          register={register}
+          errors={errors}
+         
+        />
+        <InputLogin
+          maxLength={20}
+          minLength={3}
+          required={true}
+          register={register}
+          inputName="password"
+          name="Contraseña"
+          type="password"
+          styleLabel="labelLogin"
+          errors={errors}
+          
+        />
+        <div className={styles["container-buttons"]}>
+          <Button type={"submit"} value="Iniciar" style="buttonIniciarSesion">
+            <img src={flechaButton} alt="icono flecha derecha" />{" "}
+          </Button>
 
-        <Button onClick={handleClick} value="Registrar" />
-      </div>
-    </form>
+          <Button onClick={handleClick} value="Registrar" />
+        </div>
+      </form>
+      <Alerts error={error.error} mensaje={error.mensaje} onClose={onClose} errorType="error" />
+    </>
   );
 };
 
