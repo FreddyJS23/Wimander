4ff4358da56641ef4b75a38f751d154f56256f6d
Freddy Solarte <freddyjsolarte@gmail.com>
2023-07-28 15:38:50 -0400

    feat: logica de formulario al registrar usuarios
    uso de reach hook form
    
    estado para controlar las alertas
    
    validaciones y respuestas de validacion del servidor

--- src/components/FormRegistroUsuario.tsx
+++ src/components/FormRegistroUsuario.tsx
@@ -1,82 +1,140 @@
-import { RegistroLogin } from "../types";
+import { RegisterUserForm, alertState } from "../types";
 import CamposForm from "./CamposForm";
 import { useState } from "react";
 import styles from "../styles/login.module.css";
 import Button from "./Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Alerts from "./Alerts";
+import { createUser } from "../services/user";
+import { GetErrorsResponse } from "../utils/GetErrorsResponse";
 
 export const FormRegistroUsuario = () => {
-  const initialState = {
-    nombre: "",
-    apellido: "",
-    correo: "",
-    usuario: "",
-    password: "",
-    password2: "",
+  //control de formulario
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<RegisterUserForm>({ defaultValues: { name: "", email: "" } });
+
+  //estado de la alerta
+  const [alertOpen, setAlertOpen] = useState<alertState>({
+    open: false,
+    mensaje: "",
+    tipo: "success",
+  });
+
+  //logica validacion de registro
+  const onSubmit: SubmitHandler<RegisterUserForm> = async (form, e) => {
+ //comprobar si contraeñas son iguales
+    if (getValues("password") != getValues("password2"))
+      return setAlertOpen({
+        open: true,
+        mensaje: "Las contraseñas no coinciden",
+        tipo: "error",
+      });
+
+    const { data, status } = await createUser(form);
+    const { errors } = data;
+//obtener errores de los campos del servidor
+    if (GetErrorsResponse(errors))
+      return setAlertOpen({
+        open: true,
+        mensaje: GetErrorsResponse(errors),
+        tipo: "error",
+      });
+    else if (status == 201) {
+      e?.target.reset();
+      setAlertOpen({ open: true, mensaje: "usuario creado", tipo: "success" });
+    }
   };
 
-  const [form, setForm] = useState<RegistroLogin>(initialState);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = () => {
-    return;
+  //cierre de alerta
+  const onClose = () => {
+    setAlertOpen({ ...alertOpen, open: false });
   };
 
   return (
-    <form action="" className={styles["formLoginRegistro"]}>
-      <CamposForm
-        type="text"
-        value={form.nombre}
-        handleChange={handleChange}
-        id="nombre"
-        name="Nombre"
-        styleLabel="labelLogin"
-      />
-      <CamposForm
-        type="text"
-        value={form.apellido}
-        handleChange={handleChange}
-        id="apellido"
-        name="Apellido"
-        styleLabel="labelLogin"
-      />
-      <CamposForm
-        type="text"
-        value={form.correo}
-        handleChange={handleChange}
-        id="correo"
-        name="Correo"
-        styleLabel="labelLogin"
-      />
-      <CamposForm
-        type="text"
-        value={form.usuario}
-        handleChange={handleChange}
-        id="usuario"
-        name="Usuario"
-        styleLabel="labelLogin"
-      />
-      <CamposForm
-        type="password"
-        value={form.password}
-        handleChange={handleChange}
-        id="password"
-        name="Contraseña"
-        styleLabel="labelLogin"
-      />
-      <CamposForm
-        type="password"
-        value={form.password2}
-        handleChange={handleChange}
-        id="password2"
-        name="Repetir contraseña"
-        styleLabel="labelLogin"
-      />
+    <>
+      <form
+        action=""
+        className={styles["formLoginRegistro"]}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <CamposForm
+          type="text"
+          register={register}
+          inputName="name"
+          name="Nombre"
+          styleLabel="labelLogin"
+          errors={errors}
+          minLength={3}
+          maxLength={25}
+          tip={"Minimo 3 caracteres y maximo 25 "}
+        />
+        <CamposForm
+          type="text"
+          register={register}
+          inputName="last_name"
+          name="Apellido"
+          styleLabel="labelLogin"
+          errors={errors}
+          minLength={3}
+          maxLength={25}
+          tip={"Minimo 3 caracteres y maximo 25 "}
+        />
+        <CamposForm
+          type="text"
+          register={register}
+          inputName="email"
+          name="Correo"
+          styleLabel="labelLogin"
+          errors={errors}
+          pattern={/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/}
+          tip={"Debe ser un correo valido"}
+        />
+        <CamposForm
+          type="text"
+          register={register}
+          inputName="user"
+          name="Usuario"
+          styleLabel="labelLogin"
+          errors={errors}
+          minLength={3}
+          maxLength={15}
+          tip={"Minimo 3 caracteres y maximo 15 "}
+        />
+        <CamposForm
+          type="password"
+          register={register}
+          inputName="password"
+          name="Contraseña"
+          styleLabel="labelLogin"
+          errors={errors}
+          minLength={7}
+          maxLength={15}
+          tip={"Minimo 7 caracteres y maximo 25 "}
+        />
+        <CamposForm
+          type="password"
+          register={register}
+          inputName="password2"
+          name="Repetir contraseña"
+          styleLabel="labelLogin"
+          errors={errors}
+          minLength={7}
+          maxLength={15}
+          tip={"Deben coincidir"}
+        />
 
-      <Button onClick={handleSubmit} value="Registrar" type={"submit"} />
-    </form>
+        <Button value="Registrar" type={"submit"} />
+      </form>
+      <Alerts
+        open={alertOpen.open}
+        tipo={alertOpen.tipo}
+        onClose={onClose}
+        mensaje={alertOpen.mensaje}
+      />
+    </>
   );
 };
