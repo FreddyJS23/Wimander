960ed87d673ea96b2c9b431ea365214240bd8976
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-06 22:44:10 -0400

    refactor: change interface
    se estrajo la interfaz en su respectiva ubicacion ademas que se añadio una nueva prop register para el control del campo

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,24 +1,19 @@
 import style from "../styles/radioButton.module.css";
+import { RadioButtonInterface } from "../types";
 
-interface RadioButtonInterface {
-  titulo: string;
-  name: string;
-  value: string | number;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
 
 const RadioButton = ({
   titulo,
   name,
   value,
-  handleChange,
+  register
 }: RadioButtonInterface) => {
   return (
     <div className={style["container"]}>
       <label className={style["cl-checkbox"]}>
-        <input onChange={handleChange} name={name} value={value} type="radio" />
+        <input {...register(name,{required:true})} value={value} type="radio" />
         <span>
-          <span className={style["texto"]}>{titulo}</span>{" "}
+          <span className={style["texto"]}>{titulo}</span>
         </span>
       </label>
     </div>
