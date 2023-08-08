77d576af1eb3631755d493ff37ec9d85d226305d
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-08 19:10:54 -0400

    feat:add componente boton crear cliente

---
+++ src/components/BotonCrearCliente.tsx
@@ -0,0 +1,19 @@
+import icono from "../assets/plus-icon.svg";
+import style from "../styles/botonCrearCliente.module.css";
+
+interface Props {
+  onClick: () => void;
+}
+
+const BotonCrearCliente = ({ onClick }: Props) => {
+  return (
+    <button onClick={onClick} className={style["boton"]}>
+      <div className={style["container-icono"]}>
+        <img src={icono} alt="salir" />
+      </div>
+      <p className={style["texto"]}>Crear cliente</p>
+    </button>
+  );
+};
+
+export default BotonCrearCliente;
