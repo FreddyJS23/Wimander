0659d4104e1482385a48da8b941dfc8ade8c352d
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-10 15:07:45 -0400

    feat: add service para logout

---
+++ src/services/logout.ts
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { Response } from "../types/index"
+
+
+export const Logout = ():Promise<Response> => {
+
+    return axios.get(`${import.meta.env.VITE_API_URL}logout`)
+        .then(res => {
+            const { data, status }: Response = res
+            return { data, status, }
+        }
+        )
+        .catch(err => {
+            const { data, status }: Response = err.response ? err.response : { data: null, status: 408 }
+            return { data, status }
+
+        })
+}
