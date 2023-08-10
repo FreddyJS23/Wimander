c512bc7d6ad908da70df1ef24af73c5465dbf3a9
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-10 15:04:09 -0400

    feat: add nuevo servicios para usuario
    obtencion y actualizacion

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { RegisterUserForm, Response } from "../types/index"
+import { RegisterUserForm, Response, UserUpdateForm } from "../types/index"
 
 export const createUser= (form: RegisterUserForm) => {
 
@@ -15,3 +15,41 @@ export const createUser= (form: RegisterUserForm) => {
 
         })
 }
+/**Obtener un usuario
+ * @param user - id del user a solicitar 
+ */
+export const GetUser= (user:number | string | undefined) => {
+
+    return axios.get(`${import.meta.env.VITE_API_URL}user/${user}`)
+        .then(res => {
+            const { data, status }: Response = res
+            return { data, status, }
+        }
+        )
+        .catch(err => {
+            const { data, status }: Response = err.response ? err.response : { data:{errors:'Sin conexion al servidor'}, status: 408 }
+            return { data, status }
+
+        })
+}
+
+
+/**Actualizar usuario
+ * @param cliente - id del usuario a modificar 
+ * @param form - formulario con los campos a modificar 
+ */
+export const ActualizarUser= (user:number | string | undefined,form:UserUpdateForm) => {
+    //eliminar campo id del formulario
+     delete form.id
+      return axios.put(`${import.meta.env.VITE_API_URL}user/${user}`,form)
+          .then(res => {
+              const { data, status }: Response = res
+              return { data, status, }
+          }
+          )
+          .catch(err => {
+              const { data, status }: Response = err.response ? err.response : { data:{errors:'Sin conexion al servidor'}, status: 408 }
+              return { data, status }
+ 
+          })
+ }
