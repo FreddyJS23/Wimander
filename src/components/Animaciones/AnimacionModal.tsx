279b7f81464c9dd103a61af0b7451d1e0aa82965
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-13 14:35:20 -0400

    fix: change animacion modal
    ahora se hace uso de animacion de material ui, se elimino la animacion que se tenia con frame motion debido a la complejida que representa, ya que hay coordinar el desmonte del modal y la ejecucion de la animacion, anque se conserva el componente

--- src/components/Animaciones/AnimacionModal.tsx
+++ src/components/Animaciones/AnimacionModal.tsx
@@ -1,27 +1,40 @@
+import { forwardRef } from "react";
 import { AnimacionModalInterface } from "../../types/index";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence} from "framer-motion";
 
 /* para animacion de salidas hay que envolver el div de la animacion con "AnimatePresence" 
 y pasar una prop de apertura de la animacion la animacion tiene 3 estado los cuales son
  el estado initial indica los valores iniciales del contenido, animate los valores que se animaran, 
 y exit los valores de la animacion de salida */
 
-export const AnimacionModal = ({
-  children,
-  in: open,
-}: AnimacionModalInterface) => {
-  return (
+export const AnimacionModal =forwardRef<HTMLDivElement,AnimacionModalInterface> (function AnimacionModal(props,ref){
+
+  const {
+    children,
+    in: open,
+    onClick,
+    onEnter,
+    onExited,
+    ownerState,
+    
+  } = props;
+  
+return (
     <AnimatePresence>
-      {open && (
-        <motion.div
-          initial={{ opacity: 0, y:-20 }}
+      {open && <motion.div
+        
+        initial={{ opacity: 0, y:-20 }}
           animate={{ opacity: 1, y:0 }}
           exit={{ opacity: 0, y:-20, }}
-          transition={{  duration: 0.3 }}
+          transition={{  duration: 1 }}
+          
         >
           {children}
-        </motion.div>
-      )}
+        </motion.div>    }
+
+        
+  
     </AnimatePresence>
   );
-};
+
+  })
--- src/components/Modales/ModalBase.tsx
+++ src/components/Modales/ModalBase.tsx
@@ -1,28 +1,27 @@
-import { Modal } from "@mui/material";
+import { Modal, Zoom } from "@mui/material";
 import { ModalBaseInterface } from "../../types";
-import { AnimacionModal } from "../Animaciones";
+//import { AnimacionModal } from "../Animaciones";
 import style from "../../styles/modales.module.css";
 import cerrarIcon from "../../assets/cerrar.svg";
 
 /** estrucura principal del modal */
 export const ModalBase = ({
-    children,
-    encabezado,
-    open,
-    handleClose,
-  }: ModalBaseInterface) => {
-    return (
-      <Modal open={open} closeAfterTransition onClose={handleClose}>
-        <AnimacionModal in={open}>
-          <div className={`${style["container-modal"]} `}>
-            <div className={style["container-encabezado"]}>
-              <p>{encabezado}</p>
-              <img onClick={handleClose} src={cerrarIcon} alt="cerrar" />
-            </div>
-            <div className={style["container-cuerpo"]}>{children}</div>
+  children,
+  encabezado,
+  open,
+  handleClose,
+}: ModalBaseInterface) => {
+  return (
+    <Modal open={open} closeAfterTransition onClose={handleClose}>
+      <Zoom in={open} >
+        <div className={`${style["container-modal"]} `}>
+          <div className={style["container-encabezado"]}>
+            <p>{encabezado}</p>
+            <img onClick={handleClose} src={cerrarIcon} alt="cerrar" />
           </div>
-        </AnimacionModal>
-      </Modal>
-    );
-  };
-  
+          <div className={style["container-cuerpo"]}>{children}</div>
+        </div>
+      </Zoom>
+    </Modal>
+  );
+};
