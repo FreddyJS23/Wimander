bfb5f98d492d930126bdf99a51bcaeecfc83685f
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-13 14:45:37 -0400

    feat: add conversion y formateo para fechas
    asignacion de tipo y convercion de las fechas, ahora se añade una clase de error y advertencia en caso que la fecha esten a punto de caudicar

--- src/components/Tablas/columns/columCustomer.ts
+++ src/components/Tablas/columns/columCustomer.ts
@@ -1,24 +1,36 @@
-import { GridColDef} from "@mui/x-data-grid";
+import { GridColDef } from "@mui/x-data-grid";
+import { milisecondsToDay } from "../../../utils/converts";
 
+export const columCustomer: GridColDef[] = [
+  { field: "id", headerName: "ID", flex: 1, editable: false, minWidth: 70 },
+  { field: "name", headerName: "Nombre", flex: 1, minWidth: 70 },
+  { field: "last_name", headerName: "Apellido", flex: 0.8, minWidth: 70 },
+  { field: "mac", headerName: "Mac", flex: 1, minWidth: 70 },
+  {
+    field: "start_date",
+    headerName: "Fecha de inicio",
+    type: "date",
+    valueGetter: (params) => new Date(params.value),
+    sortable: false,
+    flex: 0.9,
+    minWidth: 70,
+  },
+  {
+    field: "expiration_date",
+    headerName: "Fecha de expiracion",
+    sortable: false,
+    flex: 0.9,
+    type: "date",
+    valueGetter: (params) => new Date(params.value),
+    minWidth: 70,
+    cellClassName: ({ value }) => {
+      let styleDateAlert = ''
 
-export const columCustomer:GridColDef[]=[
-    { field: "id", headerName: "ID", flex: 1, editable: false, minWidth: 70 },
-    { field: "name", headerName: "Nombre", flex: 1, minWidth: 70 },
-    { field: "last_name", headerName: "Apellido", flex: 0.8, minWidth: 70 },
-    { field: "mac", headerName: "Mac", flex: 1, minWidth: 70 },
-    {
-      field: "start_date",
-      headerName: "Fecha de inicio",
-      sortable: false,
-      flex: 0.9,
-      minWidth: 70,
-    },
-    {
-      field: "expiration_date",
-      headerName: "Fecha de expiracion",
-      sortable: false,
-      flex: 0.9,
-      minWidth: 70,
-    },
-    
-  ];
+      if (milisecondsToDay(value) <= 7 && milisecondsToDay(value) > 2) styleDateAlert = 'dateOrange'
+
+      else if (milisecondsToDay(value) <= 2) styleDateAlert = 'dateRed'
+
+      return styleDateAlert
+    }
+  }
+];
