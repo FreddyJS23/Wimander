ca43880505675b3dcd8d7be06e694ea50a36c77f
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-17 10:17:46 -0400

    feat: add modal configuraciones inicial layout
    se solicita las confuguraciones del usuario, en caso que no tenga se abre un modal para pedir configuraciones inciales

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useContext, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/navs/Sidebar";
 import style from "../styles/layout.module.css";
@@ -8,19 +8,38 @@ import { AnimacionesLayout } from "../components/Animaciones";
 import { ButtonBurger } from "../components/Botones";
 import { Drawer } from "@mui/material";
 import { useState } from "react";
+import { GetConfigs } from "../services/config";
+import { ConfigsContext } from "../context/configurations";
+import { ModalSettings } from "../components/Modales/ModalSettings";
 
 const Layout = () => {
   const [sidebarResponsive, setSidebarResponsive] = useState<boolean>(false);
   const [cheked, setCheked] = useState(false);
-
+  //revisar si existen configuraciones inciales
+  const [configsInitials, setConfigsInitials] = useState(false)
+ 
   //cierre de sidebar responsive
   const handleClick = () => {
     setSidebarResponsive(!sidebarResponsive);
     setCheked(!cheked);
   };
+  const handleClose=()=>setConfigsInitials(false)
+  const{setConfigs}=useContext(ConfigsContext)
 
+  useEffect(() => {
+    const request=async()=>{
+      const {data,status}=await GetConfigs()
+     if( status == 200 ) 
+    { setConfigs(data.configs)
+    setConfigsInitials(false)
+    }else setConfigsInitials(true)
+    }
+   request()
+  }, [setConfigs])
+  
   return (
-    <div className={style["container-principal"]}>
+   
+  ( configsInitials ? <ModalSettings encabezado="Configuracion inicial" open={configsInitials}  handleClose={handleClose}  /> :  <div className={style["container-principal"]}>
       <header>
         <Sidebar />
         {/* nav responsive */}
@@ -44,7 +63,7 @@ const Layout = () => {
           </AnimacionesLayout>
         </Suspense>
       </main>
-    </div>
+    </div>   )
   );
 };
 
