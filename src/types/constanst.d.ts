12bbe4e1471432fe768207c6a5dc285cd55ad988
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-19 14:54:59 -0400

    refactor: add interfaces para constantes

--- src/types/constanst.d.ts
+++ src/types/constanst.d.ts
@@ -1,4 +1,6 @@
 import { AlertState } from ".";
 
+/**Estado actual de la alerta */
 export type AlertStateConst=Omit<AlertState,'mensaje'> 
-  
+/**Mensaje de la alerta */
+export type AlertStateMsj=Pick<AlertState,'mensaje'> 
