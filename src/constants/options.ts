1068ccd75aca2576527ed2015c0c7b1cbdb0e6f5
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-19 23:36:49 -0400

    refactor: add constante para credenciales

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -1,4 +1,4 @@
 export const TIMED_CLOSE_MODAL = 500
 export const TIMED_ANIMATION_MODAL = 350
-export const CREDITIANLS={user:'admin', password:'admin'}
+export const CREDITIANLS={user:'Admin', password:'Admin'}
 export const USER_SESSION={ id: 20, name: 'Administrador', token: "token" }
