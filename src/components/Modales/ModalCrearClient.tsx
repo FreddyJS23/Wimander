224c127b4c402566ce04729907a268fe4d63705a
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-21 16:25:31 -0400

    fix: change estilo label de inputs tipo date

--- src/components/Modales/ModalCrearClient.tsx
+++ src/components/Modales/ModalCrearClient.tsx
@@ -97,6 +97,7 @@ export const ModalCrearClient = ({
             name="Inicio del plan"
             required={true}
             errors={errors}
+            styleLabel={'label_select'}
           />
 
           <div className={style["container-plan"]}>
