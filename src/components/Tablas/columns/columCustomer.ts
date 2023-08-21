1ce552044a193ae25ded805edb8828fa13e3d2be
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-21 16:13:03 -0400

    feat: delete colunma id

--- src/components/Tablas/columns/columCustomer.ts
+++ src/components/Tablas/columns/columCustomer.ts
@@ -2,7 +2,6 @@ import { GridColDef } from "@mui/x-data-grid";
 import { milisecondsToDay } from "../../../utils/converts";
 
 export const columCustomer: GridColDef[] = [
-  { field: "id", headerName: "ID", flex: 1, editable: false, minWidth: 70 },
   { field: "name", headerName: "Nombre", flex: 1, minWidth: 70 },
   { field: "last_name", headerName: "Apellido", flex: 0.8, minWidth: 70 },
   { field: "mac", headerName: "Mac", flex: 1, minWidth: 70 },
